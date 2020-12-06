e29d7fee9b119fa4ee994b5e5f7b4141e38b7fcf
PhiBot <75005262+teaphi@users.noreply.github.com>
2020-12-06 11:07:23 +0800

    Update fucktime.py

--- fucktime.py
+++ fucktime.py
@@ -1,11 +1,1 @@
-import os
-import random
-
-time = "{].{}"
-date = "{}/{}/{}"
-
-
-
-while True:
-	os.system("time " + time.format(str(random.randint(0, 23)), str(random.randint(0,59))))
-	os.system("date " + date.format(str(random.randint(2000, 2077)), str(random.randint(1,12)), str(random.randint(1,29))))
+print("警告！请先预览README文件及免责声明 https://github.com/PhiTea/py-fucktime/")
